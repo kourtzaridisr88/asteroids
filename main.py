56a9c1a958c6ebf5959880e09093f6b79a63b658
George Kourtzaridis <w84it@hackthebox.eu>
2024-10-24 00:37:08 +0300

    draw player

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
 
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0    
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)   
     while True:
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
  
+        
         screen.fill("black")
+        
+        player.draw(screen)
         pygame.display.flip()
+        last_time = clock.tick(60)
+        dt = last_time / 1000
+
+        # render player
+
+
 if __name__ == "__main__":
    main()
